9cf50da8483240a9ef2203ecd96201fe95265e13
Rahul Singh <rahul@Rahuls-MacBook-Pro.local>
2019-03-25 02:49:58 +0530

    cc

---
+++ server/router/routes/apiv1.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Rahul Singh
+ */
+var express = require('express');
+var router = express.Router();
+//********************** */
+
+//Controllers
+
+var video = require('../../controllers/video');
+var playlist = require('../../controllers/playlist');
+
+
+//Path Routes
+router.get('/', function (req, res, next) {
+    res.json('Welcome.. Smart Audit happens here !! ');
+});
+
+// Videos Api :
+router.get('/allvideo', video.getAllVideos);
+router.get('/video', video.getVideosID);
+router.get('/video_remove', video.videoRemoveID);
+router.post('/addVideo', video.addVideo);
+
+//playlist Api :
+router.post('/addplaylist', playlist.addplaylist);
+router.get('/allplaylists', playlist.getAllPlaylists);
+router.get('/playlist', playlist.getPlaylistID);
+router.get('/playlist_remove', playlist.playlistRemoveID);
+
+
+
+
+module.exports = router;
